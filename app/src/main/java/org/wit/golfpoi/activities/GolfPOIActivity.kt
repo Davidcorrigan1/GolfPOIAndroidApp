35b009183a39397c02148036313fd353334523ca
David Corrigan <davidcorrigan1@gmail.com>
2021-09-29 15:21:42 +0100

    Menu items to add Course and Cancel

--- app/src/main/java/org/wit/golfpoi/activities/GolfPOIActivity.kt
+++ app/src/main/java/org/wit/golfpoi/activities/GolfPOIActivity.kt
@@ -2,7 +2,10 @@ package org.wit.golfpoi.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
+import org.wit.golfpoi.R
 import org.wit.golfpoi.databinding.ActivityGolfpoiBinding
 import org.wit.golfpoi.main.MainApp
 import org.wit.golfpoi.models.GolfPOIModel
@@ -18,6 +21,10 @@ class GolfPOIActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityGolfpoiBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Bind the toolbar and set the title
+        binding.toolbarAdd.title = title
+        setSupportActionBar(binding.toolbarAdd)
 
         Timber.plant(Timber.DebugTree())
 
@@ -35,6 +42,8 @@ class GolfPOIActivity : AppCompatActivity() {
                 for (i in app.golfPOIs.indices) {
                     i("Golf Course [$i] : ${app.golfPOIs[i]}")
                 }
+                setResult(RESULT_OK)
+                finish()
             } else {
                 Snackbar
                     .make(it, "Please enter a Title and Desc", Snackbar.LENGTH_LONG)
@@ -42,5 +51,21 @@ class GolfPOIActivity : AppCompatActivity() {
             }
 
         }
+    }
+
+    // Inflate the menu
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_golfpoi, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // Handle the clicking of the menu cancel item. Return to previous activity.
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_cancel -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/org/wit/golfpoi/activities/GolfPOIListActivity.kt
+++ app/src/main/java/org/wit/golfpoi/activities/GolfPOIListActivity.kt
@@ -1,7 +1,10 @@
 package org.wit.golfpoi.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import org.wit.golfpoi.R
 import org.wit.golfpoi.adapter.GolfPOIAdapter
@@ -15,13 +18,40 @@ class GolfPOIListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Inflate the layout and Bind it to the activity
         binding = ActivityGolfPoilistBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Enable action bar and give it a title
+        binding.toolbar.title = title
+        setSupportActionBar(binding.toolbar)
+
+        // Initialise the app object
         app = application as MainApp
 
+        // Set the recyclerView layout and link the adapter
         val layoutManager = LinearLayoutManager(this)
         binding.recyclerView.layoutManager = layoutManager
         binding.recyclerView.adapter = GolfPOIAdapter(app.golfPOIs)
+
+
+    }
+
+    // Overload method to load the menu resource
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // Implements a menu event handler; if the event is item_add then the GolfPOIActivity is started
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_add -> {
+                val launcherIntent = Intent(this, GolfPOIActivity::class.java)
+                startActivityForResult(launcherIntent,0)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
